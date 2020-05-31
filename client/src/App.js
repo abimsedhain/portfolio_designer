1a5c501294492007cbe998fc47abbb5772ed7988
abim <apexabim09@gmail.com>
2020-05-31 13:58:10 +0545

    .

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
     <Router>
     <div className="App">
       <AppNavbar/>
+      <Jumbotronn/>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
