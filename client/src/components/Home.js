7b23c40d08ad0765fdc64a9b6293659f9c0a7840
abim <apexabim09@gmail.com>
2020-05-31 13:51:38 +0545

    added reactstrap

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            Lorem Ipsum
+          <div>
+             Lorem Ipsum
 "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
 "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
 
