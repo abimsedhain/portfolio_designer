7a0bba5d39292ed78607ec56f6ae7bfbdbe7a822
abim <apexabim09@gmail.com>
2020-06-11 23:11:42 +0545

    New design for user detail form

--- src/Form/PersonalInfo.js
+++ src/Form/PersonalInfo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import { Label, Input, Button, FormGroup } from "reactstrap"
+import './Form.css';
 
 const validationSchema = yup.object({
 	// fullName: yup.string().required('Name is required').max(20),
@@ -25,23 +26,27 @@ const PersonalInfo = ({ formData, setFormData, nextStep }) => {
 				<h1>Personal Information</h1>
 
 				<FormGroup>
-					<Label>Full Name</Label>
+					{/* <Label>Full Name</Label> */}
 					<Field type="text" id="fullName" name="fullName" as={Input} placeholder="Enter Full Name"></Field>
 					<ErrorMessage name='fullName' />
 				</FormGroup>
 
 				<FormGroup>
-					<Label>Email</Label>
-					<Field type='email' id='email' name='email' as={Input} placeholder="Enter Email"/>
+					{/* <Label>Email</Label> */}
+					<Field type='email' id='email' name='email' as={Input} placeholder="Enter Email" />
 					<ErrorMessage name='email' />
 				</FormGroup>
 
 				<FormGroup>
-					<Label>Bio</Label>
-					<Field as='textarea' rows='5' id='bio' name='bio' as={()=>(<Input type="textarea"></Input>)} placeholder="Enter Bio"/>
+					{/* <Label>Bio</Label> */}
+					<Field rows='5' id='bio'name='bio' as={() => (<Input type="textarea" rows="7" placeholder="Enter your Bio Here..."></Input>)}/>
 					<ErrorMessage name='email' />
 				</FormGroup>
-				<Button type='submit'> NEXT </Button>
+{/* 
+				<FormGroup>
+					<input type="input" class="form__field" placeholder="Enter Full Name" name="name" id='name' />
+				</FormGroup> */}
+				<Button type='submit'> Continue </Button>
 			</Form>
 		</Formik>
 
