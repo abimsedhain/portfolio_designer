c008f5084bacfda51285442015b350017e806f3e
asimsedhain <asimsedhain33@gmail.com>
2020-06-12 01:02:49 -0500

    updates

---
+++ src/components/styled/theme.js
@@ -0,0 +1,15 @@
+const Theme={
+	Header: "Montserrat",
+	Subheader: "Cardo",
+	Description: "Yrsa",
+	HeaderSize: "2.5rem",
+	SubheaderSize: "1.75rem",
+	DescriptionSize: "1.5rem",
+	Dark: "#3b3b3b",
+	White: "#fff",
+	Primary: "#485f71",
+	Secondary: "#f4f9ff",
+	Special: "#4549b1"
+}
+
+export default Theme
