15d419bfa8eb96948e92dd3c5be159f0a9bee0f8
asimsedhain <asimsedhain33@gmail.com>
2020-06-14 03:31:50 -0500

    style(Landing Page): updated navbar and button placement.
    navbar collaspe text to the right
    button size fixed for different devices

--- src/components/styled/StyledComponents.js
+++ src/components/styled/StyledComponents.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Header = styled.h1`
 	font-family: ${props => props.theme.Header};
-	font-size: ${props => props.Small? props.theme.SubheaderSize: props.theme.HeaderSize};
+	font-size: ${props => props.Small ? props.theme.SubheaderSize : props.theme.HeaderSize};
 	font-weight: ${props => props.theme.HeaderWeight};
 	color: ${props => props.White ? props.theme.White : props.theme.Dark}
 `
@@ -10,7 +10,7 @@ const Header = styled.h1`
 const Logo = styled.h2`
 	font-family: ${props => props.theme.Header};
 	font-weight: ${props => props.theme.HeaderSize};
-	font-size: ${props => props.Small? props.theme.SmallDescriptionSize: props.theme.LogoSize};
+	font-size: ${props => props.Small ? props.theme.SmallDescriptionSize : props.theme.LogoSize};
 	color: ${props => props.White ? props.theme.White : props.theme.Dark}
 `
 
@@ -21,33 +21,39 @@ const Subheader = styled.h2`
 `
 const Description = styled.p`
 	font-family: ${props => props.theme.Description};
-	font-size: ${props => props.Small? props.theme.SmallDescriptionSize: props.theme.DescriptionSize};
+	font-size: ${props => props.Small ? props.theme.SmallDescriptionSize : props.theme.DescriptionSize};
 	color: ${props => props.White ? props.theme.White : props.theme.Dark}
 `
 const Button = styled.button`
-	font-size: ${props=> props.theme.ButtonSize};
+	font-size: ${props => props.theme.ButtonSize};
 	font-size: ${props => props.Small && props.theme.SmallButtonSize};
-	font-size: ${props=> props.Large && props.theme.LargeButtonSize};
+	font-size: ${props => props.Large && props.theme.LargeButtonSize};
 	display: inline-block;
-	color: ${props=>props.White? props.theme.White: props.theme.Dark};
+	color: ${props => props.White ? props.theme.White : props.theme.Dark};
 	margin: 1em;
 	padding: 0.25em 1em;
 	border: 0px solid;
 	border-radius: 50px;
-	background-color: ${props=>props.theme.Primary};
-	background-color: ${props=> props.Secondary && props.theme.Secondary};
+	background-color: ${props => props.theme.Primary};
+	background-color: ${props => props.Secondary && props.theme.Secondary};
 	outline: none;
 	&:focus{
 		outline: none;
 	}	
 	&:hover{
-		background-color:${props=>props.theme.Special};
-		color: ${props=>props.theme.White};
+		background-color:${props => props.theme.Special};
+		color: ${props => props.theme.White};
 	}
 `
 
 const NavbarButton = styled(Button)`
-	margin-top: 5px;
+	margin: 0em;	
+	font-size: 1.2rem;
+	@media (min-width: 768px) {
+		margin: 1em;
+		margin-top: 5px;
+		font-size:1.1rem;
+	 }
 `
 
-export { Header, Logo, Subheader, Description, Button, NavbarButton}
+export { Header, Logo, Subheader, Description, Button, NavbarButton }
