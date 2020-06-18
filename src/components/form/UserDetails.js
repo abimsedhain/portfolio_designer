6be99a52911320cbd167ccae18f66056b555bdf9
asimsedhain <asimsedhain33@gmail.com>
2020-06-18 04:02:45 -0500

    feat(form): fields are hidden if they are empty

--- src/components/form/UserDetails.js
+++ src/components/form/UserDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useContext } from 'react';
-import {Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import { Route, useHistory } from 'react-router-dom';
 import PersonalInfo from './PersonalInfo';
 import Projects from './Projects';
@@ -12,13 +12,15 @@ import LogoBar from '../LogoBar';
 import Template from '../templates/Template';
 
 import { FormContainer } from '../styled/StyledComponents';
-import {userContext} from "../utility/UserContext";
+import { userContext } from "../utility/UserContext";
 
 function UserDetails({ match }) {
 	const history = useHistory()
-	const [formData, setFormData] = useContext(userContext);	
+	const [formData, setFormData] = useContext(userContext);
 	// Creating an array for the components and react element array. then returning the selected array
-	const FormComponents = useRef([PersonalInfo, SocialMedia, Projects, Experience, Education, Review, Submit])
+	// const FormComponents = useRef([PersonalInfo, SocialMedia, Projects, Experience, Education, Review, Submit])
+
+	const FormComponents = useRef([PersonalInfo, Education, SocialMedia, Experience, Projects, Review, Submit])
 
 
 	return (
@@ -28,7 +30,7 @@ function UserDetails({ match }) {
 				<Row>
 					<Col className="mx-auto col-12 col-sm-8 col-md-6">
 						<FormContainer style={{ height: '85vh', overflow: 'scroll' }}>
-						<Route path={`${match.path}/:formId?`} render={(props) => {
+							<Route path={`${match.path}/:formId?`} render={(props) => {
 								const matchInner = props.match
 								const id = parseInt(matchInner.params.formId) || 0
 								return React.createElement(FormComponents.current[id], { formData, setFormData, nextStep: () => history.push(`${match.url}/${id + 1}`), prevStep: () => history.goBack() })
@@ -37,9 +39,9 @@ function UserDetails({ match }) {
 					</Col>
 
 					<Col className="mx-auto col-12 col-sm-8 col-md-6">
-							<FormContainer style={{ height: '85vh', overflow: 'scroll' }}>
+						<FormContainer style={{ height: '85vh', overflow: 'scroll' }}>
 							<Template />
-							</FormContainer>
+						</FormContainer>
 					</Col>
 				</Row>
 			</FormContainer>
--- src/components/templates/Template.js
+++ src/components/templates/Template.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Theme from "./styled/Theme";
 import styled, { ThemeProvider } from "styled-components";
 import { Container, Col, Row } from "reactstrap";
@@ -15,7 +15,7 @@ import twitter from '../../assets/twit.svg'
 import linkedin from '../../assets/linkedin.svg'
 import mail from '../../assets/email.svg'
 
-import {userContext} from "../utility/UserContext"
+import { userContext } from "../utility/UserContext"
 // For testing purpose only
 // import user from "./TempUser"
 // For testing purpose only
@@ -25,26 +25,27 @@ const linkIcons = [fb, github, twitter, linkedin, mail]
 
 
 const StyledImg = styled.img`
-	filter: ${props=> props.theme.PrimaryFilter}
+	filter: ${props => props.theme.PrimaryFilter}
 `
 
 
 function Template() {
 
-	const [user, setUser] = useContext(userContext);
+	const [formData, setFormData] = useContext(userContext);
 	// console.log(formData)
 	// console.log(hexToFilter("#000000"))
 	return (<ThemeProvider theme={Theme}>
-		<TemplateNavbar/>
+		<TemplateNavbar />
 		<Container className="text-left py-5">
 			<Row>
 				<Col >
-					<Header Bold>hi, i'm <ColoredSpan Primary>{user.fullName}</ColoredSpan>.</Header>
+					<Header Bold>hi, i'm <ColoredSpan Primary>{formData.fullName}</ColoredSpan>.</Header>
 				</Col>
 			</Row>
 			<Row>
-				<Col ><Subheader White>{user.bio.toLocaleLowerCase()}</Subheader>
-				</Col>
+				{/* Checks if there is any empty string and only prints non empty objects */}
+				{formData.bio.trim().length > 0 ? (<Col ><Subheader White>{formData.bio.toLocaleLowerCase()}</Subheader>
+				</Col>) : <></>}
 			</Row>
 		</Container>
 		<Container className="text-left pt-5" id="education">
@@ -53,7 +54,8 @@ function Template() {
 					<Subheader Primary>education</Subheader>
 				</Col>
 			</Row>
-			{user.education.map((edu, index) => (<EducationItem key={index} education={edu} />))}
+			{/* Checks if there is any empty string and only prints non empty objects */}
+			{formData.education.filter(edu => Object.values(edu).reduce((acc, value) => acc || (typeof (value) === "string" ? value.trim() : false), false)).map((edu, index) => (<EducationItem key={index} education={edu} />))}
 		</Container>
 		<Container className="text-left pt-5" id="skills">
 			<Row className="pb-5 pt-3">
@@ -62,7 +64,7 @@ function Template() {
 				</Col>
 			</Row>
 			<Row>
-				{user.skills.map((skill, index) => (<SkillItem key={index} skill={skill} />))}
+				{formData.skills.filter(skill=>skill.trim().length>0).map((skill, index) => (<SkillItem key={index} skill={skill} />))}
 			</Row>
 		</Container>
 		<Container className="text-left pt-5" id="experiences">
@@ -71,7 +73,7 @@ function Template() {
 					<Subheader Primary>work experiences</Subheader>
 				</Col>
 			</Row>
-			{user.experiences.map((experience, index) => (<WorkItem key={index} experience={experience} />))}
+			{formData.experiences.filter(exp => Object.values(exp).reduce((acc, value) => acc || (typeof (value) === "string" ? value.trim() : false), false)).map((experience, index) => (<WorkItem key={index} experience={experience} />))}
 		</Container>
 		<Container className="text-left pt-5" id="projects">
 			<Row className="pb-5 pt-3">
@@ -79,14 +81,14 @@ function Template() {
 					<Subheader Primary>projects</Subheader>
 				</Col>
 			</Row>
-			{user.projects.map((project, index) => (<ProjectItem key={index} project={project} />))}
+			{formData.projects.filter(project => Object.values(project).reduce((acc, value) => acc || (typeof (value) === "string" ? value.trim() : false), false)).map((project, index) => (<ProjectItem key={index} project={project} />))}
 		</Container>
 		<Container className="pt-5" id="contact">
 			<Row className="justify-content-center pt-5">
-				{linkIcons.map((icon, index)=>(<Col className="col-2 col-sm-1" key={index}>
-							<StyledImg className="img-fluid" height="100%" width="100%" src={icon} alt={`icon_${index}`}/>
-						</Col>))}
-				</Row>
+				{linkIcons.map((icon, index) => (<Col className="col-2 col-sm-1" key={index}>
+					<StyledImg className="img-fluid" height="100%" width="100%" src={icon} alt={`icon_${index}`} />
+				</Col>))}
+			</Row>
 
 			<Row className="pt-5">
 				<Col>
