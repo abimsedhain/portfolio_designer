4ead566d54e0b8a8e80baea55a3c7dfc5c6f672e
asimsedhain <asimsedhain33@gmail.com>
2020-06-20 21:16:45 -0500

    fix: empty highlights removed

--- src/components/utility/reducer.js
+++ src/components/utility/reducer.js
@@ -3,7 +3,7 @@ import types from "./types"
 const reducer = (state, action) => {
 	switch (action.type) {
 		case types.PREVIEW:
-			return isEmpty(action.payload)? state: removeEmptyFields(action.payload)
+			return isEmpty(action.payload) ? state : removeEmptyFields(action.payload)
 		default:
 			return state
 	}
@@ -11,7 +11,7 @@ const reducer = (state, action) => {
 
 
 const isEmpty = (value) => {
-	if(value === null || value === undefined){
+	if (value === null || value === undefined) {
 		return true
 	} else if (typeof (value) === "string") {
 		return value.trim() === ""
@@ -20,15 +20,28 @@ const isEmpty = (value) => {
 	}
 }
 
-const removeEmptyFields = (payload)=>{
+const removeEmptyFields = (payload) => {
 	let tempState = {}
 
 	const keys = Object.keys(payload)
 	const values = Object.values(payload)
 
-	for(let index in keys){
-		if(!isEmpty(values[index])){
+	for (let index in keys) {
+		if (!isEmpty(values[index])) {
 			tempState[keys[index]] = values[index]
+			if (keys[index] === "projects" || keys[index] === "experiences") {
+				for (let indexJ in values[index]) {
+					tempState[keys[index]][indexJ].Highlights = values[index][indexJ].Highlights.filter((value) => !isEmpty(value))
+				}
+			}
+			if (keys[index] === "skills") {
+				tempState[keys[index]] = values[index].filter((value) => !isEmpty(value))
+			}
+			if(keys[index]==="education"){
+				for (let indexJ in values[index]) {
+					tempState[keys[index]][indexJ].Courses = values[index][indexJ].Courses.filter((value) => !isEmpty(value))
+				}
+			}
 		}
 	}
 	return tempState
