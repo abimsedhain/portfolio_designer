81299cb920fae01bfdd6244a7c2ce322028c0bdb
Ashim Sedhain <asimsedhain33@gmail.com>
2020-06-27 22:47:16 -0500

    fix: changed a tag to link in navbar

--- src/components/LogoBar.js
+++ src/components/LogoBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Logobar} from './styled/StyledComponents';
+import {Link} from "react-router-dom"
 
 function AppNavbar() {
 
 	return (
 		<>
+		
 		<Logobar Primary fluid={true}>
-			<h1><a href="/" style={{color:'white', textDecoration:'none'}}>Capos</a></h1>
+			<h1><Link to="/" style={{color:'white', textDecoration:'none'}}>Capos</Link></h1>
 		</Logobar>
 		
 		</>
