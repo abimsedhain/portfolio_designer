148f8c998e57fa6dac6f9a5c32d89d5a775ad5c1
asimsedhain <asimsedhain33@gmail.com>
2020-06-30 22:09:31 -0500

    style(Homepage button): removed text decoration from homepage button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,82 +1,92 @@
 import React, { useState, useContext } from "react";
 import { userStateContext } from "../state management/userContext";
 import { Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import AppNavbar from "./AppNavbar";
 import mainlogo from "../assets/logo2.svg";
 import {
-  Header,
-  Description,
-  HomepageButton,
-  GoogleSignInButton,
-  FackbookSignInButton,
-  FullSizeContainer,
+	Header,
+	Description,
+	HomepageButton,
+	GoogleSignInButton,
+	FackbookSignInButton,
+	FullSizeContainer,
 } from "./styled/StyledComponents";
 
 function Home() {
-  const userState = useContext(userStateContext).userState;
-  const [showLoginCard, setShowLoginCard] = useState(false);
-  return (
-    <>
-      <AppNavbar setShowLoginCard={setShowLoginCard} />
-      <FullSizeContainer>
-        <Row className="py-5">
-          <Col className="text-left col-12 col-sm-12 col-md-6 col-lg-7 order-md-0 order-sm-1 order-xs-1 order-1 pt-5 pt-md-0">
-            <Header>Capos</Header>
-            <Description id="LoginCard">
-              Build a simple and elegant Portfolio Website under 10 minutes.
-            </Description>
-            {showLoginCard ? (
-              <>
-                <GoogleSignInButton
-                  White
-                  Small
-                  Block
-                  onClick={() =>
-                    window.location.replace(
-                      process.env.REACT_APP_GOOGLE_AUTH_URL
-                    )
-                  }
-                >
-                  Login with Google
-                </GoogleSignInButton>
-                <FackbookSignInButton White Small Block>
-                  Login with Facebook
-                </FackbookSignInButton>
-              </>
-            ) : (
-              <>
-                <Link to="/form">
-                  <HomepageButton White Small Block>
-                    Let's Build a Portfolio
-                  </HomepageButton>
-                </Link>
-                {userState.name === "" ? (
-                  <HomepageButton
-                    White
-                    Small
-                    Block
-                    onClick={() => {
-                      setShowLoginCard(true);
-                    }}
-                  >
-                    Login/Signup
-                  </HomepageButton>
-                ) : (
-                  <HomepageButton White Small Block>
-                    Go to Dashboard
-                  </HomepageButton>
-                )}
-              </>
-            )}
-          </Col>
-          <Col className="d-none d-sm-none d-md-block">
-            <img src={mainlogo} alt="logo" className="img-fluid"></img>
-          </Col>
-        </Row>
-      </FullSizeContainer>
-    </>
-  );
+	const userState = useContext(userStateContext).userState;
+	const [showLoginCard, setShowLoginCard] = useState(false);
+	const history = useHistory();
+	return (
+		<>
+			<AppNavbar setShowLoginCard={setShowLoginCard} />
+			<FullSizeContainer>
+				<Row className="py-5">
+					<Col className="text-left col-12 col-sm-12 col-md-6 col-lg-7 order-md-0 order-sm-1 order-xs-1 order-1 pt-5 pt-md-0">
+						<Header>Capos</Header>
+						<Description id="LoginCard">
+							Build a simple and elegant Portfolio Website under
+							10 minutes.
+						</Description>
+						{showLoginCard ? (
+							<>
+								<GoogleSignInButton
+									White
+									Small
+									Block
+									onClick={() =>
+										window.location.replace(
+											process.env
+												.REACT_APP_GOOGLE_AUTH_URL
+										)
+									}
+								>
+									Login with Google
+								</GoogleSignInButton>
+								<FackbookSignInButton White Small Block>
+									Login with Facebook
+								</FackbookSignInButton>
+							</>
+						) : (
+							<>
+								<HomepageButton
+									White
+									Small
+									Block
+									onClick={() => history.push("/form")}
+								>
+									Let's Build a Portfolio
+								</HomepageButton>
+								{userState.name === "" ? (
+									<HomepageButton
+										White
+										Small
+										Block
+										onClick={() => {
+											setShowLoginCard(true);
+										}}
+									>
+										Login/Signup
+									</HomepageButton>
+								) : (
+									<HomepageButton White Small Block>
+										Go to Dashboard
+									</HomepageButton>
+								)}
+							</>
+						)}
+					</Col>
+					<Col className="d-none d-sm-none d-md-block">
+						<img
+							src={mainlogo}
+							alt="logo"
+							className="img-fluid"
+						></img>
+					</Col>
+				</Row>
+			</FullSizeContainer>
+		</>
+	);
 }
 
 export default Home;
