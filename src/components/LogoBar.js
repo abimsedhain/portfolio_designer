3ae964bc0d785d73fa514e0bfe6d5c88eb37afd7
abim <apexabim09@gmail.com>
2020-07-02 10:15:10 +0545

    refactor: removed inline css

--- src/components/LogoBar.js
+++ src/components/LogoBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Logobar} from './styled/StyledComponents';
-import {Link} from "react-router-dom"
+import { Logobar, LogoText } from './styled/StyledComponents';
+import { Link } from "react-router-dom"
 
 function AppNavbar() {
 
 	return (
 		<>
-		
-		<Logobar Primary fluid={true}>
-			<h1><Link to="/" style={{color:'white', textDecoration:'none'}}>Capos</Link></h1>
-		</Logobar>
-		
+			<Logobar Primary fluid={true}>
+				<Link to="/">
+					<LogoText>
+						Capos
+					</LogoText>
+				</Link>
+			</Logobar>
 		</>
-        
 	)
 }
 
