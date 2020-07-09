e568f7300b88c6061be5109e02d04a02c4889679
asimsedhain <asimsedhain33@gmail.com>
2020-07-09 00:12:15 -0500

    fix(unused varible): remove unused varible

--- src/components/FormFooter.js
+++ src/components/FormFooter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Navbar, Nav, NavItem } from 'reactstrap';
+import { Container} from 'reactstrap';
 
 import { StyledFooter, FooterButton } from "./styled/StyledComponents";
 
