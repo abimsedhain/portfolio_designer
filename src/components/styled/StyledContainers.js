d2f5d29e6fe632b49a7ed89d4daf96ef8b8979cb
asimsedhain <asimsedhain33@gmail.com>
2020-07-14 20:48:35 -0500

    style: update form height to better fit mobile

--- src/components/styled/StyledContainers.js
+++ src/components/styled/StyledContainers.js
@@ -52,7 +52,7 @@ const StyledGrid = styled.div`
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	grid-template-rows: max-content auto;
-	height: calc(100vh - 60px);
+	height: calc(100vh - 55px);
 `;
 
 const StyledGridElement = styled.div`
